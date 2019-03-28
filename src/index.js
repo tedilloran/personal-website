0a602538aa3a013ff391a2e4d4693e61ce091b90
Trent Edilloran <trent.edilloran@gmail.com>
2019-03-28 04:23:53 -0700

    Cleaned up redux and router components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
-import reducers from './reducers';
 import App from './components/App.js';
 
-const store = createStore(reducers);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
