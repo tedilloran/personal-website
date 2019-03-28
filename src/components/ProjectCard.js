771daf08ad792527da60fda756061652f9ec11f9
Trent Edilloran <trent.edilloran@gmail.com>
2019-03-28 03:06:27 -0700

    Reorder project cards.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,43 +1,22 @@
 import React from 'react';
-import { Modal, Card, Image, Button } from 'semantic-ui-react';
+import { Modal, Card, Image, Icon, Button } from 'semantic-ui-react';
 
 import '../styles/projectCard.css';
 
 class ProjectCard extends React.Component {
-  state = { modalOpen: false };
-
-  handleOpen = () => this.setState({ modalOpen: true });
-
-  handleClose = () => this.setState({ modalOpen: false });
-
   render() {
     return (
-      <Modal
-        size={'mini'}
-        className='card-modal'
-        open={this.state.modalOpen}
-        onClose={this.handleClose}
-        trigger={
-          <Card onClick={this.handleOpen}>
-            <Card.Content>
-              <Image floated='left' size='mini' circular src={this.props.imgUrl} />
-              <Card.Header>{this.props.name}</Card.Header>
-              <Card.Description>{this.props.description}</Card.Description>
-            </Card.Content>
-            <Card.Content extra>Github link</Card.Content>
-          </Card>
-        }>
-        <Modal.Header>{this.props.name}</Modal.Header>
-        <Modal.Content image>
-          <Image wrapped size='medium' src={this.props.imgUrl} />
-          <Modal.Description>{this.props.description}</Modal.Description>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button basic onClick={this.handleClose}>
-            Close
-          </Button>
-        </Modal.Actions>
-      </Modal>
+      <Card>
+        <Card.Content>
+          <Image floated='left' size='mini' circular src={this.props.imgURL} />
+          <Card.Header>{this.props.name}</Card.Header>
+          <Card.Description>{this.props.description}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Icon name='chain' />
+          <a href={this.props.githubURL}>GitHub Repository</a>
+        </Card.Content>
+      </Card>
     );
   }
 }
--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -19,27 +19,32 @@ class ProjectCards extends React.Component {
           <ProjectCard
             name='Steam Web Scraper'
             description='Web scraper for obtaining game information from Steam store.'
-            imgUrl={process.env.PUBLIC_URL + '/assets/pictures/steam.png'}
-          />
-          <ProjectCard
-            name='Tetris Clone'
-            description='Javascript implementation of Tetris rendered within a browser.'
-            imgUrl={process.env.PUBLIC_URL + '/assets/pictures/tetris.png'}
-          />
-          <ProjectCard
-            name='Shell Prompt'
-            description='Fully functional multi-threaded shell with full I/O redirection.'
-            imgUrl={process.env.PUBLIC_URL + '/assets/pictures/terminal.png'}
-          />
-          <ProjectCard
-            name='Robot Arm Controller'
-            description='Robotic joint controller for simulated movement on 2D plane.'
-            imgUrl={process.env.PUBLIC_URL + '/assets/pictures/robot.png'}
+            imgURL={process.env.PUBLIC_URL + '/assets/pictures/steam.png'}
+            githubURL='https://github.com/tedilloran/Steam-Web-Scraper'
           />
           <ProjectCard
             name='Passwd as a Service'
             description='Network fetcher and parser for ‘/etc/passwd’ and ‘/etc/group’ files.'
-            imgUrl={process.env.PUBLIC_URL + '/assets/pictures/network.png'}
+            imgURL={process.env.PUBLIC_URL + '/assets/pictures/network.png'}
+            githubURL='https://github.com/tedilloran/passwdService'
+          />
+          <ProjectCard
+            name='Tetris Clone'
+            description='Javascript implementation of Tetris rendered within a browser.'
+            imgURL={process.env.PUBLIC_URL + '/assets/pictures/tetris.png'}
+            githubURL='https://github.com/tedilloran/tetrisJS'
+          />
+          <ProjectCard
+            name='Robotic Joint Controller'
+            description='Robotic joint controller for simulated movement on 2D plane.'
+            imgURL={process.env.PUBLIC_URL + '/assets/pictures/robot.png'}
+            githubURL='https://github.com/tedilloran/Robotic-Arm-Actuator'
+          />
+          <ProjectCard
+            name='Shell Prompt'
+            description='Fully functional multi-threaded shell with full I/O redirection.'
+            imgURL={process.env.PUBLIC_URL + '/assets/pictures/terminal.png'}
+            githubURL='https://github.com/tedilloran/Shell'
           />
         </Card.Group>
       </div>
